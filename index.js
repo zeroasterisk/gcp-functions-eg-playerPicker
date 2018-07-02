2ce2f7cb7ff0219b7dccad58f93f1f8245fc91e9
alan bount <alan@zeroasterisk.com>
2018-07-02 08:49:13 -0600

    clean non-alphanumeric char from players inputs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 exports.playerPicker = (req, res) => {
-  const players = _.isArray(req.body) ? req.body : [];
+  const players = (_.isArray(req.body) ? req.body : [])
+    .map(x => x.replace(/[^a-zA-Z0-9\.\-\_]/g, ''));
   if (players.length === 1) {
     const player = _.first(players);
     res.send(`Only Player: ${player}`);
